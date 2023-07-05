63be8537b27395119b203de42d0b8f318149706a
RAHUL KHALLAR <khallarrahul@gmail.com>
2023-07-05 13:30:28 -0700

    resolves

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,39 @@
 #!/usr/bin/env python3
 
+
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+
+
+greet_programmer()
+
 
 def greet(name):
-    pass
+    print(f"Hello, ${name}!")
+
+
+greet("Naureen")
+
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, ${name}!")
+
+
+greet_with_default("Sunny")
+
 
 def add(num1, num2):
-    pass
+    print(num1 + num2)
+    return num1 + num2
+
+
+add(1, 2)
+
 
 def halve(number):
-    pass
+    if type(number) != int and type(number) != float:
+        return None
+    return number / 2
+
+
+print(halve(4))
